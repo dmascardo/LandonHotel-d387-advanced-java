5c2b7e59bc5c3efa35d7cbd0cede0cf82fad99b2
Denise Rose <dmascardo@cnm.edu>
2025-10-22 13:18:45 -0600

    rebuild

--- src/main/java/edu/wgu/d387_sample_code/controller/DisplayMessageController.java
+++ src/main/java/edu/wgu/d387_sample_code/controller/DisplayMessageController.java
@@ -14,11 +14,11 @@ public class DisplayMessageController {
 
     @GetMapping("/api/info")
     public String getDisplayInfo() throws InterruptedException {
-        // Create welcome message tasks
+
         DisplayMessage englishTask = new DisplayMessage(Locale.ENGLISH);
         DisplayMessage frenchTask = new DisplayMessage(Locale.FRENCH);
 
-        // Run in parallel threads
+
         Thread enThread = new Thread(englishTask);
         Thread frThread = new Thread(frenchTask);
 
@@ -29,17 +29,17 @@ public class DisplayMessageController {
         frThread.join();
 
 
-        // Get formatted messages
+
         String englishMessage = englishTask.getMessage();
         String frenchMessage = frenchTask.getMessage();
 
 
 
-        // Get current ET time and convert to MT and UTC
+
         ZonedDateTime nowET = ZonedDateTime.now(ZoneId.of("America/Toronto"));
         String timeOutput = TimeZoneConversion.convertTimeZones(nowET);
 
-        // Return formatted HTML response
+
         return timeOutput + "<br>" + englishMessage + "<br>" + frenchMessage;
     }
 }
